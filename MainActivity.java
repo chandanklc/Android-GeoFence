fe786c78b53935781cf8f6325c22089fc6853c72
chandanklc <64265063+chandanklc@users.noreply.github.com>
2020-07-30 11:19:53 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.constrainlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }}
